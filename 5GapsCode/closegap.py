bc6e28c7ef235bf7e3f01564e616aea4c6ca2e34
Brian Sutjiadi <bsutjiad@ucsd.edu>
2016-09-30 15:41:09 -0700

    Implemented threshold gap size calculation
    Implemented functionality to determine the threshold gap size based on the
    within-gene peak gap distances. Also restructured some of the data being passed
    around to allow for easier gap joining later.

--- 5GapsCode/closegap.py
+++ 5GapsCode/closegap.py
@@ -1,19 +1,31 @@
 #!/usr/env/python
 
 import re
+import numpy
+import math
 
 
 def main():
-    #ann_file = open("/home/brian/genes_cpy.txt", 'r')
-    #seq_file = open("./data/GM12_ENCFF439LIW_rRNA_minus_RNA_calledRegions_peaks.bed")
+    ann_file = open("/home/brian/genes_cpy.txt", 'r')
+    seq_file = open("./data/GM12_ENCFF439LIW_rRNA_minus_RNA_calledRegions_peaks.bed")
 
-    #ann_list, seq_list = parse(ann_file, seq_file)
-    ann_list = [(11, 15, "chr1"), (2, 8, "chr1"), (33, 70, "chr1"), (15, 500, "chr2")]
-    seq_list = [(3, 4, "chr1"), (5, 6, "chr1"), (8, 9, "chr1"), (13, 14, "chr1"), (15, 17, "chr1"), (22, 27, "chr1"), (40, 50, "chr1"), (55, 60, "chr1"), (7, 8, "chr1"), (70, 80, "chr1"), (333, 400, "chr2"), (450, 475, "chr2"), (42, 56, "chr3")]
+    ann_list, seq_list = parse(ann_file, seq_file)
+    #ann_list = [(11, 15, "chr1"), (2, 8, "chr1"), (33, 70, "chr1"), (15, 500, "chr2")]
+    #seq_list = [(3, 4, "chr1"), (5, 6, "chr1"), (8, 9, "chr1"), (13, 14, "chr1"), (15, 17, "chr1"), (22, 27, "chr1"), (40, 50, "chr1"), (55, 60, "chr1"), (7, 8, "chr1"), (70, 80, "chr1"), (333, 400, "chr2"), (450, 475, "chr2"), (42, 56, "chr3")]
     ann_chroms, seq_chroms = get_chroms(ann_list, seq_list)
     diffs = get_differences(ann_chroms, seq_chroms)
-    print diffs
-    #compute_stats(diffs)
+
+    # Determine threshold gap size
+    numpyarr = numpy.array(diffs)
+    numpyarr_sorted = numpy.sort(numpyarr)
+    arrlen = numpy.size(numpyarr_sorted)
+    threshold_index = int(math.floor(0.1*arrlen)) # 10% threshold
+    threshold_gapsize = numpyarr_sorted[threshold_index]
+    print "Threshold gap size is: {}".format(threshold_gapsize)
+
+    #print
+    #for item in seq_chroms:
+    #    print seq_chroms[item]
 
     #for item in ann_list:
     #    print item
@@ -21,8 +33,8 @@ def main():
     #for item in seq_list:
     #    print item
 
-    #ann_file.close()
-    #seq_file.close()
+    ann_file.close()
+    seq_file.close()
 
 
 # Input: Annotation file (ann_file), Sequence/.bed file (seq_file)
@@ -63,7 +75,7 @@ def get_chroms(ann_list, seq_list):
     ann_chroms = {}
     seq_chroms = {}
     for item in ann_list:
-        ann_s, ann_e, ann_c = item
+        ann_s, ann_e, ann_c, _ = item
         if ann_chroms.get(ann_c) == None:
             ann_chroms[ann_c] = []
             ann_chroms[ann_c].append((ann_s, ann_e))
@@ -73,9 +85,9 @@ def get_chroms(ann_list, seq_list):
         seq_s, seq_e, seq_c = item
         if seq_chroms.get(seq_c) == None:
             seq_chroms[seq_c] = []
-            seq_chroms[seq_c].append((seq_s, seq_e))
+            seq_chroms[seq_c].append((seq_s, seq_e, False, seq_c))
         else:
-            seq_chroms[seq_c].append((seq_s, seq_e))
+            seq_chroms[seq_c].append((seq_s, seq_e, False, seq_c))
 
     # Order the distinct lists by start position
     for item in ann_chroms:
@@ -83,6 +95,8 @@ def get_chroms(ann_list, seq_list):
     for item in seq_chroms:
         seq_chroms[item] = sorted(seq_chroms[item], key=lambda x:x[0])
 
+    # Note: The tuples in seq_chroms contain extra information needed for
+    #       downstream processing: (start, end, within_gene_peak_bool, chr#)
     return ann_chroms, seq_chroms
 
 # Input: Dictionaries containing chromosome positional information
@@ -113,12 +127,13 @@ def get_differences(ann_chroms, seq_chroms):
                 break
 
             ann_s, ann_e = chr_ann_list[ann_index]
-            seq_s, seq_e = chr_seq_list[seq_index]
+            seq_s, seq_e, _, seq_c = chr_seq_list[seq_index]
 
             if seq_s < ann_s:
                 seq_index = seq_index + 1
             elif seq_e <= ann_e:
                 gene_peaks.append(chr_seq_list[seq_index])
+                chr_seq_list[seq_index] = (seq_s, seq_e, True, seq_c)
                 seq_index = seq_index + 1
             else:
                 for diff in compute_differences(gene_peaks):
@@ -140,11 +155,24 @@ def compute_differences(lst):
     index = 0
     results = []
     while index < size-1:
-        _, first_e = lst[index]
-        last_s, _ = lst[index+1]
+        _, first_e, _, _ = lst[index]
+        last_s, _, _, _ = lst[index+1]
         results.append(last_s - first_e)
         index = index + 1
     return results
+
+
+
+# Input: 2 peak tuples (start, end, chrom)
+# Output: 1 peak tuple (start, end, chrom)
+# Note: Assumes we are linking end of peak1 to start of peak2. Assumes peaks are
+#       from the same chromosome
+def join(peak1, peak2):
+    p1_s, _, chrom = peak1
+    _, p2_e, _ = peak2
+
+    ret_peak = (p1_s, p2_e, chrom)
+
 
 if __name__ == "__main__":
     main()
