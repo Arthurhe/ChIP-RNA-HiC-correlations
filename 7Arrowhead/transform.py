6ad212270c48255f8d3e144251a8b56a0a3e0ea6
Brian Sutjiadi <bsutjiad@ucsd.edu>
2016-10-30 19:06:55 -0700

    Implemented the arrowhead transformation
    Currently, the transformation utilizes numpy ndarrays for all operations and
    computations. The transformation involves computing the horizontal distance of
    the current index to the diagonal, and then using that distance to access values
    that are that distance away from the diagonal. Any distance for that goes out
    of the matrix bounds is ignorred and the value in the transformed matrix is 0.

---
+++ 7Arrowhead/transform.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import numpy
+
+
+def main():
+    # read original matrix from file
+    datamatr = numpy.loadtxt("testdata.txt")
+    nrow, ncol = datamatr.shape
+
+    # Transformed matrix zero-initialized
+    transmat = numpy.zeros((nrow, ncol))
+    tnrow, tncol = datamatr.shape
+
+    # iterate through all elements in the matrix and transform
+    it = numpy.nditer(datamatr, flags=['multi_index'])
+    while not it.finished:
+        currow, curcol = it.multi_index
+        d = abs(currow - curcol)
+        lindex = currow - d
+        rindex = currow + d
+        onedindex = (currow * ncol) + curcol
+
+        # only compute if both left and right indices exist
+        if lindex >= 0 and rindex < ncol:
+            lval = datamatr.item(currow, lindex)
+            rval = datamatr.item(currow, rindex)
+            numer = lval - rval
+            denom = lval + rval
+            tval = numer / denom
+            numpy.put(transmat, onedindex, tval)
+
+        it.iternext()
+
+    print(transmat)
+    # Note, you may need to adjust the float precision of the output to save
+    # save space.
+    numpy.savetxt("transformed.txt", transmat, fmt='%.13f', delimiter="\t")
+
+if __name__ == "__main__":
+    main()
