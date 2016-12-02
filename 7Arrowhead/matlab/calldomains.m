27b89868cc3b7f05d79c3dc3795c5f37e9bf655e
Brian Sutjiadi <bsutjiad@ucsd.edu>
2016-12-01 18:06:12 -0800

    Bugfixes for matlab function
    The function now returns 1 index for each connected component. Still not sure
    how I will write these results to a file...

--- 7Arrowhead/matlab/calldomains.m
+++ 7Arrowhead/matlab/calldomains.m
@@ -10,13 +10,13 @@ filteredMatr = dlmread(filteredMatrFname);
 % get connected components
 [L, num] = bwlabel(binMatr);
 
-domain_boundaries_r = []
-domain_boundaries_c = []
+domain_boundaries_r = zeros(num, 1);
+domain_boundaries_c = zeros(num, 1);
 
 % iterate through every connected component
 for ccnum = 1:1:num
     % get the indices of pixels in that component
-    [r, c] = find(L=ccnum);
+    [r, c] = find(L == ccnum);
 
     % for every idex
         % check the corresponding value in the the filtered matrix
@@ -31,9 +31,9 @@ for ccnum = 1:1:num
     end
 
     % save the index of the max value
-    domain_boundaries_r(end + 1) = maxR;
-    domain_boundaries_c(end + 1) = maxC;
+    domain_boundaries_r(ccnum) = maxR;
+    domain_boundaries_c(ccnum) = maxC;
 end
 
 % write the list of indices to a file, or return them as output
-wut = [domain_boundaries_r domain_boundaries_c]
+wut = [domain_boundaries_r domain_boundaries_c];
