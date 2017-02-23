7e28b0a01ba83ea45c60d47457ad403fe7474160
Brian Sutjiadi <bsutjiad@ucsd.edu>
2017-02-23 15:47:45 -0800

    Completed bedtools wrapper
    Comparison filter was implmented by taking in an input file describing the
    desired comparisons. An option was added for writing output to a file instead
    of stdout.

--- 8DomainPeakCorr/bedtoolsCoverageWrapper.py
+++ 8DomainPeakCorr/bedtoolsCoverageWrapper.py
@@ -2,29 +2,57 @@
 
 import subprocess
 import argparse
-import os
+import sys
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("bedA")
     parser.add_argument("bedPath")
+    parser.add_argument("markerListFile")
+    parser.add_argument("-o", "--output", help="file to write output to. If not specified, print results to stdout")
     args = parser.parse_args()
 
-    wrapper(args.bedA, args.bedPath)
+    # Parse the markerListFile
+    # The markerListFile is a 2-column, tab-separated file describing the tag
+    # name and that tag's bed filepath. Eg.
+    # CTCF  /some/path/to/ctcf.bed
+    # SMC   /some/path/to/smc.bed
+    # H3K4  /some/path/to/H3K4.bed
+
+    f = open(args.markerListFile, 'r')
+
+    # markers is a list of marker name/filepath tuples
+    markers = []
+    for line in f:
+        line = line.rstrip()
+        sline = line.split("\t")
+        markers.append((sline[0], sline[1]))
+
+    f.close()
+
+    result = wrapper(args.bedA, args.bedPath, markers)
+
+    # Decide how to return output
+    if args.output is None:
+        for thing in result:
+            print(thing)
+    else:
+        outfile = open(args.output, 'w')
+        for thing in result:
+            outfile.write("{}\n".format(thing))
+        outfile.close()
 
 
-def wrapper(fa, bedPath):
-    # TODO: do filename filtering based on input list
-    # FIXME: for now, just call coverage on all files
-
+def wrapper(fa, bedPath, markers):
     # buff stores the coverage results
     buff = []
-    for fb in os.listdir(bedPath):
-        print("Computing converage of: {}".format(fb))
-        buff = call_bedtools_filter(fa, "{}/{}".format(bedPath, fb), buff)
+    for tupl in markers:
+        markerName, path = tupl
+        sys.stderr.write("Computing converage of: {}\n".format(path))
+        buff = call_bedtools_filter(fa, path, buff, markerName)
 
-    # Add the 'chr start end' columns to buff
+    # Output formatting - Add the 'chr start end' columns to buff
     fileA = open(fa, 'r')
     buff[0] = "chrom\tstart\tend\t" + buff[0]
     i = 1
@@ -34,12 +62,11 @@ def wrapper(fa, bedPath):
         buff[i] = sline[0] + "\t" + sline[1] + "\t" + sline[2] + "\t" + buff[i]
         i += 1
 
-    for thing in buff:
-        print(thing)
+    return buff
 
 
 # Bedtools works with filenames, not file descriptors
-def call_bedtools_filter(fa, fb, buff):
+def call_bedtools_filter(fa, fb, buff, markerName):
     # Call bedtools coverage
     proc = subprocess.Popen(["bedtools", "coverage", "-a", fa, "-b", fb],
                             stdout=subprocess.PIPE)
@@ -51,12 +78,12 @@ def call_bedtools_filter(fa, fb, buff):
 
     # Format the header
     if len(buff) == 0:
-        buff.append(fb)
+        buff.append(markerName)
     else:
-        buff[0] = buff[0] + "\t" + fb
+        buff[0] = buff[0] + "\t" + markerName
 
-    i = 1
     # Filter and format output
+    i = 1
     while True:
         line = proc.stdout.readline()
         line = line.decode("utf-8")
