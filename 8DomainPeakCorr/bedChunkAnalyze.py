7bc6070419b5c4616fbcb55ebbc2286429d85915
Brian Sutjiadi <bsutjiad@ucsd.edu>
2017-02-23 17:58:32 -0800

    Integrated the bedtools wrapper into ChunkAnalyze
    BedChunkAnalyze now divides each bed peak into smaller bins, and the coverage
    of each bin is computed by bedtools. BedChunkAnalyze then parses the results
    and "de-chunkifies" or remerges the bins back into groups representing the
    original peak. Future work will programatically create however many matrices
    necessary instead of hard coding.

--- 8DomainPeakCorr/bedChunkAnalyze.py
+++ 8DomainPeakCorr/bedChunkAnalyze.py
@@ -7,38 +7,50 @@
 #              intervals in B.
 
 import argparse
+import subprocess
+import sys
+import numpy
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("bedA")
-    parser.add_argument("bedB")
+    parser.add_argument("bedPath")
+    parser.add_argument("markerListFile")
     args = parser.parse_args()
 
-    print(args.bedA)
     bedA = open(args.bedA, 'r')
-    tmpFile = open("tmp.txt", 'w')
+    tmpFile = open("splitBed.bed", 'w')
 
-    peakId = 0
+    # Divide every peak into chunks
     for line in bedA:
-        intervalSize = chunk(line, 14, tmpFile, peakId)
-        print(intervalSize)
-        peakId += 1
+        chunk(line, 14, tmpFile)
 
     tmpFile.close()
 
-    # Call bedtools
+    # I have the split bed file - compute the coverages of that file
+    subprocess.call(["python3", "bedtoolsCoverageWrapper.py", "splitBed.bed",
+                     args.bedPath, args.markerListFile, "-o", "splitCoverages.bed"])
 
-    # TODO: delete outfile
+    # Construct matrix from each additional column in the coverage file
+    sCovFile = open("splitCoverages.bed", 'r')
+    result = parse_coverage_file(sCovFile)
+    sCovFile.close()
+    print(result)
 
-    # TODO: divide by intervalSize to normalize by size
+    # Create matrices from parsed results
+    numSubBins = len(result[0][1])
+    numBins = numSubBins // 14
+    a = numpy.asarray(result[0][1])
+    b = a.reshape((numBins, -1))
+    print(b)
 
     bedA.close()
-    pass
+    return
 
 
 # Divide each given bed peak into n bins of even size, and write bins to file
-def chunk(line, n, outFile, pId):
+def chunk(line, n, outFile):
     line = line.rstrip()
     sline = line.split("\t")
 
@@ -53,15 +65,34 @@ def chunk(line, n, outFile, pId):
     # the last bin
     while i < n:
         if i == n-1:
-            outFile.write("{}\t{}\t{}\t{}\n".format(chrom, start + i*d, end,
-                                                    pId))
+            outFile.write("{}\t{}\t{}\n".format(chrom, start + i*d, end))
         else:
-            outFile.write("{}\t{}\t{}\t{}\n".format(chrom, start + i*d,
-                                                    start + (i+1)*d-1, pId))
+            outFile.write("{}\t{}\t{}\n".format(chrom, start + i*d,
+                                                start + (i+1)*d-1))
         i += 1
 
-    # TODO: not actually necessary
-    return d
+    return
+
+
+def parse_coverage_file(covFile):
+    sys.stderr.write("Parsing the coverage data...\n")
+    header = covFile.readline()
+    header = header.rstrip()
+    numTab = header.count("\t")
+    numMarkers = numTab - 2
+    sheader = header.split("\t")
+    coverages = [("", [])] * numMarkers
+
+    for i in range(3, 3+numMarkers):
+        coverages[i-3] = (sheader[i], [])
+
+    for line in covFile:
+        line = line.rstrip()
+        sline = line.split("\t")
+        for i in range(3, 3+numMarkers):
+            coverages[i-3][1].append(sline[i])
+
+    return coverages
 
 
 if __name__ == "__main__":
